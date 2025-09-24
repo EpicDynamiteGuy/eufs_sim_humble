ce5110e7c91cf7a9b8e4de2a6e8ecae5bef55e86
EpicDynamiteGuy <epicdynamiteguy@gmail.com>
2025-09-24 03:05:04 +0100

    another type missmatch

--- eufs_rqt/src/eufs_rqt/EUFSRobotSteeringGUI.py
+++ eufs_rqt/src/eufs_rqt/EUFSRobotSteeringGUI.py
@@ -355,7 +355,7 @@ class EUFSRobotSteeringGUI(Plugin):
 
     def _on_min_linear_changed(self, value):
         self._widget.linear_slider.setMinimum(
-            value * EUFSRobotSteeringGUI.slider_factor)
+            int(value * EUFSRobotSteeringGUI.slider_factor))
 
     def _on_max_angular_changed(self, value):
         self._widget.angular_slider.setMaximum(
